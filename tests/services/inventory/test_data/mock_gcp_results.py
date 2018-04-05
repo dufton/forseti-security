6651afc98ab78708191bc0c169d6f016d46594c8
Lachlan Dufton <dufton@google.com>
2018-04-05 10:22:03 -0400

    Add test auditConfigs to mock_gcp_results

--- tests/services/inventory/test_data/mock_gcp_results.py
+++ tests/services/inventory/test_data/mock_gcp_results.py
@@ -405,6 +405,33 @@ CRM_PROJECT_IAM_POLICY_TEMPLATE = """
     "user:a_user@forseti.test"
    ]
   }}
+ ],
+ "auditConfigs": [
+  {{
+   "auditLogConfigs": [
+    {{
+     "logType": "ADMIN_READ"
+    }},
+    {{
+     "logType": "DATA_WRITE"
+    }},
+    {{
+     "logType": "DATA_READ"
+    }}
+   ],
+   "service": "allServices"
+  }},
+  {{
+   "auditLogConfigs": [
+    {{
+     "exemptedMembers": [
+      "499862534253-compute@developer.gserviceaccount.com"
+     ],
+     "logType": "ADMIN_READ"
+    }}
+   ],
+   "service": "cloudsql.googleapis.com"
+  }}
  ]
 }}
 """
@@ -455,6 +482,16 @@ CRM_ORG_IAM_POLICY = """
    "members": [
     "user:a_user@forseti.test"
    ]
+  }
+ ],
+ "auditConfigs": [
+  {
+   "auditLogConfigs": [
+    {
+     "logType": "ADMIN_READ"
+    }
+   ],
+   "service": "allServices"
   }
  ]
 }
